09f65fe744736ccbc68a0fc5fd06f4e34bf78ec9
Xuanyu Chen <256901023+Xuanyu6Chen@users.noreply.github.com>
2026-01-28 00:38:25 -0500

    Add pipeline config

---
+++ Src/factor_momentum/config.py
@@ -0,0 +1,16 @@
+# List of the Stock symbols
+TICKERS = [
+    "NVDA", "AAPL", "MSFT", "AMZN", "GOOGL", "GOOG", "META", "AVGO", "TSLA", "BRK-B",
+    "WMT", "LLY", "JPM", "V", "XOM", "JNJ", "ORCL", "MA", "MU", "COST",
+    "AMD", "ABBV", "PLTR", "HD", "BAC", "NFLX", "PG", "CVX", "KO", "GE",
+    "CSCO", "LRCX", "CAT", "MS", "GS", "PM", "IBM", "WFC", "RTX", "MRK",
+    "AMAT", "UNH", "AXP", "TMO", "MCD", "INTC", "CRM", "KLAC", "LIN", "TMUS"
+]
+# The earliest date to fetch
+START_DATE = "2016-01-01"
+
+END_DATE = None
+#path to store the raw data 
+DATA_DIR_RAW = "Data/Raw"
+#path to store the processed data 
+DATA_DIR_PROCESSED = "Data/Processed"
